3c7aec192061ce377321cce5278adb4724b82d3f
Lead Test <atoey_go@yahoo.com>
2022-03-17 16:44:20 -0400

    Committing Test2.java to BranchX

---
+++ src/Test2.java
@@ -0,0 +1,14 @@
+
+public class Test2 {
+
+	public static void main(String[] arts) {
+		
+		String hungry = "yes";
+		
+		if (hungry == "yes") 
+			System.out.println("eat something...");
+		else {
+			System.out.println("Do work.....");
+		}
+	}
+}
